af68fb3130da0a53ad98881d5e3a93f158962c2d
deepakbawacodeshastra <128479375+deepakbawacodeshastra@users.noreply.github.com>
2023-04-18 14:57:38 +0530

    created static menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-mat-static-blog';
+  showFiller = false;
 }
--- src/app/mat-common/mat-common.module.ts
+++ src/app/mat-common/mat-common.module.ts
@@ -4,11 +4,16 @@ import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const matModules = [
   MatButtonModule,
   MatIconModule,
-  MatDividerModule
+  MatDividerModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule
 ]
 
 @NgModule({
